59bc286dce8d28c33cb6be58bfeacda09cfcf9e3
Jer3myHsu <34175005+Jer3myHsu@users.noreply.github.com>
2020-04-08 14:43:45 -0400

    Fixed mobile view for the navbar

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,17 +1,31 @@
-function openMenu() {
-    document.getElementById("dropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('#userButton')) {
-      var dropdowns = document.getElementsByClassName("dropdownList");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function openNavMenu() {
+    document.getElementById("navMenu").classList.toggle("show");
+}
+
+function openAccountMenu() {
+  document.getElementById("accountMenu").classList.toggle("show");
+}
+function closeDropDown(menu) {
+  var dropdowns = document.getElementsByClassName(menu);
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('#userButton')) {
+    this.closeDropDown("accDropList");
   }
+  if (!event.target.matches('#navButton')) {
+    this.closeDropDown("navDropList");
+  }
+}
+window.onresize = function(event) {
+  if (window.innerWidth >= 1350) {
+    this.closeDropDown("navDropList");
+  }
+}
